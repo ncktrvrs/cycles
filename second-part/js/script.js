d21c7d62e34a9285f673d10ee852578603cdcc19
U <ncktrvrs@gmail.com>
2022-12-12 18:44:16 +0200

    In fourth task function was added.

--- second-part/js/script.js
+++ second-part/js/script.js
@@ -1,38 +1,47 @@
-//
+// //
 
-// 1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
-let result = '';
-for (let i = 20, max = 30; i <= max; i += 0.5) {
-  result += i + ' ';
-}
-console.log(result);
+// // 1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+// let result = '';
+// for (let i = 20, max = 30; i <= max; i += 0.5) {
+//   result += i + ' ';
+// }
+// console.log(result);
 
-// 2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
-for (let i = 10, max = 100; i <= max; i += 10) {
-  console.log(`${i}$ is ` + i * 27 + ` UAH`);
-}
+// // 2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+// for (let i = 10, max = 100; i <= max; i += 10) {
+//   console.log(`${i}$ is ` + i * 27 + ` UAH`);
+// }
 
-// 3. Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
-const n = 50;
-for (let i = 1, max = 100; i <= max && i * i <= n; i++) {
-  console.log(i);
-}
+// // 3. Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+// const n = 50;
+// for (let i = 1, max = 100; i <= max && i * i <= n; i++) {
+//   console.log(i);
+// }
 
 // 4. Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
-let isPrime;
 do {
-  isPrime = +prompt(`Enter the number`);
-} while (Number.isInteger(isPrime) == false)
+  n = +prompt(`Enter the number`);
+} while (Number.isInteger(n) == false)
 
-for (let i = 0; i <= isPrime; i++) {
-  alert('start');
-  if (isPrime % i == 0 && isPrime == i && isPrime > 1) {
-    console.log(`${isPrime} is a prime number`);
+switch (isPrime(n)) {
+  case true:
+    console.log(`${n} is a prime number`);
     break;
-  } else {
-    console.log(`${isPrime} is NOT a prime number`);
+
+  default:
+    console.log(`${n} is NOT a prime number`);
     break;
+}
+
+function isPrime(number) {
+  if (number <= 1) return false;
+  if (number == 2) return true;
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
   }
+  return true;
 }
 
 // 5. Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
